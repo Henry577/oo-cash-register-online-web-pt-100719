c5f4ff4b6169802f58f7d9325014d2f7c11efab6
Henry Allan <henryallan50@gmail.com>
2019-11-12 23:30:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   
   attr_accessor :total
   
-  def initialization(employee_discount)
+  def initialize(employee_discount = 0)
     
     @total = 0
     @employee_discount = employee_discount
