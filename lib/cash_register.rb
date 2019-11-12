5c2b68661ff171cc36dd553c5b55f0f3b03eef07
Henry Allan <henryallan50@gmail.com>
2019-11-12 23:39:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
     
   end
   
-  def add_item(title = "unknown", price = 0.0)
+  def add_item(title = "unknown", price = 0.0, quantity = 1)
     
     @@items[title] = price
     
